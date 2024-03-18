4ecb208700fc589831411c9a9e5dd122ef862bf0
JOSÉ MARTÍNEZ JUAN <jmartinez271@alu.ucam.edu>
2024-03-18 11:30:58 +0100

    creada la clase con el metodo de los nombres estaticos

--- SistemaGestionEmpleados.java
+++ SistemaGestionEmpleados.java
@@ -2,38 +2,27 @@ import java.util.Scanner;
 
 public class SistemaGestionEmpleados {
 
-    private Empleado[] empleados;
-
-    public SistemaGestionEmpleados(Empleado[] empleados) {
-        this.empleados = empleados;
-    }
-
-    public void aumentarSalario(double porcentaje) {
-        for (Empleado empleado : empleados) {
-            double nuevoSalario = empleado.salario * (1 + porcentaje / 100);
-            empleado.salario = nuevoSalario;
-        }
-    }
-
+    
     public static void main(String[] args) {
-        Empleado[] empleados = new Empleado[3];
-        empleados[0] = new Empleado("Juan", "Desarrollador", 50000);
-        empleados[1] = new Empleado("María", "Diseñadora", 45000);
-        empleados[2] = new Empleado("Pedro", "Gerente", 60000);
+        final String empleadoJ="Juan";
+        final  String empleadoM="María";
+        final  String empleadoP="Pedro";
+        final String cargo1="Desarrollador";
+        final String cargo2="Diseñadora";
+       final String cargo3="Gerente";
+       Empleado empleado1 = new Empleado("Juan", "Desarrollador", 50000);
+        Empleado empleado2 = new Empleado("María", "Diseñadora", 45000);
+       Empleado empleado3 = new Empleado("Pedro", "Gerente", 60000);
 
-        SistemaGestionEmpleados sistema = new SistemaGestionEmpleados(empleados);
+       
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Introduzca el porcentaje de aumento de salario: ");
         double porcentaje = scanner.nextDouble();
 
-        sistema.aumentarSalario(porcentaje);
         
-        System.out.println("Lista de Empleados:");
-        for (Empleado empleado : empleados) {
-            System.out.println(empleado);
-        }
-
+        
+       
         scanner.close();
     }
 }
---
+++ TextosFijos.java
@@ -0,0 +1,10 @@
+public class TextosFijos {
+public static void magicStings(){
+    final String empleadoJ="Juan";
+    final  String empleadoM="María";
+    final  String empleadoP="Pedro";
+    final String cargo1="Desarrollador";
+    final String cargo2="Diseñadora";
+   final String cargo3="Gerente";
+}
+}
